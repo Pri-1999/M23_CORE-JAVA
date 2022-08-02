8b66c31e6e64a2b3348b3d0e456f4a9f9ec45d35
hp <hp@Priyanka>
2022-08-02 15:47:13 +0530

    second commit

--- Day-2 Introduction/src/com/capgemini/introduction/Byte.java
+++ Day-2 Introduction/src/com/capgemini/introduction/Byte.java
@@ -1,9 +1,12 @@
 package com.capgemini.introduction;
 
-public class Main {
+public class Byte {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		byte a=127;
+		System.out.println(a);
+		
 
 	}
 
